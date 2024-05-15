838c694a1b8bcb343ac7910725ccf3b5c8d35ab4
JessicaaSun <sunjessica05@gmail.com>
2024-05-15 12:47:53 +0700

    add try catch

--- src/main/java/com/example/seleniumtesting/service/ScraperService.java
+++ src/main/java/com/example/seleniumtesting/service/ScraperService.java
@@ -2,6 +2,7 @@ package com.example.seleniumtesting.service;
 
 import lombok.AllArgsConstructor;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
@@ -19,21 +20,25 @@ public class ScraperService {
     public void scrapeFromUserInput() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the URL to scrape: ");
-        String url = scanner.nextLine();
-        driver.get(url);
-        WebElement table = driver.findElement(By.cssSelector(TABLE_SELECTOR));
+        try {
+            System.out.print("Enter the URL to scrape: ");
+            String url = scanner.nextLine();
+            driver.get(url);
+            WebElement table = driver.findElement(By.cssSelector(TABLE_SELECTOR));
 
-        List<WebElement> rows = table.findElements(By.tagName("tr"));
-        for (WebElement row : rows) {
-            List<WebElement> cells = row.findElements(By.tagName("td"));
-            for (WebElement cell : cells) {
-                System.out.print(cell.getText() + "\t");
+            List<WebElement> rows = table.findElements(By.tagName("tr"));
+            for (WebElement row : rows) {
+                List<WebElement> cells = row.findElements(By.tagName("td"));
+                for (WebElement cell : cells) {
+                    System.out.print(cell.getText() + "\t");
+                }
+                System.out.println();
             }
-            System.out.println();
+        } catch (NoSuchElementException e) {
+            System.out.println("Table not found.");
+        } finally {
+            driver.quit();
+            scanner.close();
         }
-        driver.quit();
-
-        scanner.close();
     }
 }
